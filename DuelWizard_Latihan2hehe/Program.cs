cb2ab5c5ce224c673adc5cb65ec9ecb65036e8e5
Ary-27 <aryatamaary@gmail.com>
2026-02-06 15:01:23 +0700

    NihPart2

--- DuelWizard_Latihan2hehe/Program.cs
+++ DuelWizard_Latihan2hehe/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Wizard wizardA = new Wizard("Aryzz", 20);
+Wizard wizardB = new Wizard("Kokomi", 1000);
+
+Console.WriteLine("Permainan Dimulai...\n");
+Console.WriteLine("Statistik Awal");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+wizardA.Attack(wizardB);
+wizardB.Attack(wizardA);
+
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+public class Wizard
+{
+    //Field
+    public string Name;
+    public int Energy;
+    public int Damage;
+
+
+    //Constructor
+    public Wizard(string name, int damage)
+    {
+        Name = name;
+        Energy = 100;
+        Damage = damage;
+    }
+
+    //Deklarasi Method
+    public void ShowStats()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Energy: {Energy}\n");
+    }
+
+    public void Attack(Wizard enemyObj)
+    {
+        enemyObj.Energy -= Damage;
+        Console.WriteLine($"{Name} menyerang {enemyObj.Name}");
+        Console.WriteLine($"Sisa energy {enemyObj.Name}: {enemyObj.Energy}\n");
+    }
+}
